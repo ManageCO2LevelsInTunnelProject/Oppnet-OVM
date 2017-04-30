1b61af2bf3b5b5a8ab040019ad631b6df3a17333
ManageCO2LevelsInTunnelProject <488project@gmail.com>
2017-05-01 01:11:07 +0300

    Add files via upload

---
+++ Help.java
@@ -0,0 +1,5 @@
+package pack;
+
+public class Help {
+String helpTextMsg = "Warning: The level of CO2 is high in this area. It’s more than 0.6%";
+}
---
+++ Lite.java
@@ -0,0 +1,11 @@
+package pack;
+
+public class Lite extends Helper{
+
+	public Lite(String name, Parameters p, double msgPayloadReadTime) {
+		super(name, p, msgPayloadReadTime);
+		this.msgPayloadReadtime = msgPayloadReadTime;
+}
+
+}
+
