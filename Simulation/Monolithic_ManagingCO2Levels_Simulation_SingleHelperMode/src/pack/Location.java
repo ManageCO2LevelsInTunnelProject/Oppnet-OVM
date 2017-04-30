ec93c08c78dc436e1975bbd18703c6afc0bb04fd
ManageCO2LevelsInTunnelProject <488project@gmail.com>
2017-05-01 01:22:21 +0300

    Add files via upload

---
+++ Simulation/Monolithic_ManagingCO2Levels_Simulation_SingleHelperMode/src/pack/Location.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack;
+
+
+public class Location {
+	public double X;
+	public double Y;
+
+}
---
+++ Simulation/Monolithic_ManagingCO2Levels_Simulation_SingleHelperMode/src/pack/Regular_Helper.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack;
+
+
+public class Regular_Helper extends Helper{
+
+	public Regular_Helper(String name, Parameters p, double msgPayloadReadTime) {
+		super(name, p, msgPayloadReadTime);
+		this.msgPayloadReadtime = msgPayloadReadTime;
+
+
+	}
+
+	
+}
